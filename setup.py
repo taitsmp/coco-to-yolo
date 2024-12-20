11786106e218ebb37edee1168e89c9b36700e4f9
Tait Larson <telarson@gmail.com>
2024-12-20 12:27:34 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="coco-to-yolo",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     scripts=[
         'bin/coco_to_yolo.py',
