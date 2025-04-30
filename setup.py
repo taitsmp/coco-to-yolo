b4bbf8c2e4b3b66431675df6813290922fad42a6
Tait Larson <telarson@gmail.com>
2025-04-30 06:52:18 -0400

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="coco-to-yolo",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
